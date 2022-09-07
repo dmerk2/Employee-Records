5b95dcbadc8c3461c389784a2c28f76bca840cc2
Daniel Merkin <dan.merkin@gmail.com>
2022-09-07 16:13:35 -0400

    created primary key and new table

--- db/schema.sql
+++ db/schema.sql
@@ -10,11 +10,19 @@ CREATE TABLE department (
   PRIMARY KEY(id)
 );
 
+CREATE TABLE employee (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT
+);
+
 CREATE TABLE role (
   id INT,
   title VARCHAR(30),
   salary DECIMAL,
   order_details TEXT,
   department_id INT,
-  FOREIGN KEY (department_id) 
+  FOREIGN KEY (department_id)
 );
